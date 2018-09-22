ec3c981fa85213d9b66cbc81174400db6f5eb4c8
Kevin <Kevin@Panzer>
2018-09-22 22:40:59 +0200

    Merge origin/master
    Conflicts:
    	README.md

--- SentiMeter/src/sentiometer/SentiOmeter.java
+++ SentiMeter/src/sentiometer/SentiOmeter.java
@@ -13,7 +13,7 @@ public class SentiOmeter extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        //
         stage.setScene(scene);
         stage.show();
     }
